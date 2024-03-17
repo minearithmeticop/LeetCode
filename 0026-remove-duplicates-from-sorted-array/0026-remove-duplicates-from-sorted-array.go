2d73381a08829341f75c6720b08c588090349a01
mine arithmeticop <132840743+minearithmeticop@users.noreply.github.com>
2024-03-18 00:27:51 +0700

    Time: 8 ms (59.83%), Space: 4.5 MB (15.63%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.go
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.go
@@ -1,6 +1,6 @@
 func removeDuplicates(nums []int) int {
-    if len(nums) == 0 {
-        return 0
+    if len(nums) <= 1 {
+        return len(nums)
     }
 
     // Initialize pointer i
@@ -8,13 +8,16 @@ func removeDuplicates(nums []int) int {
 
     // Iterate over the array using pointer j
     for j := 1; j < len(nums); j++ {
-        // If nums[j] is different from nums[i-1], copy it to nums[i] and increment i
+        // If nums[j] is different from nums[i-1], copy it to nums[i]
         if nums[j] != nums[i-1] {
             nums[i] = nums[j]
             i++
         }
     }
+    
+    // Modify nums slice to contain only unique elements
+    nums = nums[:i]
 
-    // Return the new length of the array
-    return i
+    // Return the length of the modified nums slice
+    return len(nums)
 }
